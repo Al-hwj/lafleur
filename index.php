3eef408bb43b708744241d95507614d63debde68
Alain <al-hwj@hotmail.com>
2013-04-07 14:25:06 +0200

    base v1

--- index.php
+++ index.php
@@ -10,6 +10,15 @@
 <!DOCTYPE html>
 <html lang="fr" class="no-js"> 
 <?php
+// Afficher les erreurs à l'écran
+ini_set('display_errors', 1);
+// Enregistrer les erreurs dans un fichier de log
+ini_set('log_errors', 1);
+// Nom du fichier qui enregistre les logs (attention aux droits à l'écriture)
+ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
+// Afficher les erreurs et les avertissements
+error_reporting(e_all);
+
 require_once("util/fonctions.inc.php");
 require_once("util/class.pdoLafleur.inc.php");
 /**
